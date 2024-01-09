eb2c4a7ee4e9d768466786033bb1b9159d62124b
KapAmantay <kapyshev.2002@gmail.com>
2024-01-09 19:26:56 +0600

    Enemy AI

--- DiplomaProject/Assets/Scripts/Slime/EnemyAI.cs
+++ DiplomaProject/Assets/Scripts/Slime/EnemyAI.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
+using PixelAdv.Utils;
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private State startingState;
+    [SerializeField] private float roamingDistanceMax = 7f;
+    [SerializeField] private float roamingDistanceMin = 3f;
+    [SerializeField] private float roamingTimerMax = 2f;
+
+    private NavMeshAgent navMeshAgent;
+    private State state;
+    private float roamingTime;
+    private Vector3 roamingPosition;
+    private Vector3 staringPosition;
+
+    private enum State
     {
-        
+        Idle,
+        Roaming
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
+        state = startingState;
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                break; 
+            case State.Roaming:
+                roamingTime -= Time.deltaTime;
+                if (roamingTime < 0)
+                {
+                    Roaming();
+                    roamingTime = roamingTimerMax;
+                }
+                break;
+        }
+    }
+
+    private void Roaming()
+    {
+        staringPosition = transform.position;
+        roamingPosition = GetRoamingPosition();
+        navMeshAgent.SetDestination(roamingPosition);
+    }
+
+    private Vector3 GetRoamingPosition()
+    {
+        return staringPosition + Utils.GetRandomDir() * UnityEngine.Random.Range(roamingDistanceMin, roamingDistanceMax);
     }
 }
