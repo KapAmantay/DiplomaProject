2ec4fcdaf6bb8293c325bb965010044cf7154c49
KapAmantay <kapyshev.2002@gmail.com>
2024-01-13 01:40:10 +0600

    changes in player controller

--- DiplomaProject/Assets/Scripts/Player/PlayerController.cs
+++ DiplomaProject/Assets/Scripts/Player/PlayerController.cs
@@ -5,16 +5,21 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float moveSpeed = 5f;
 
     private PlayerInputActions playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
 
     private void Awake()
     {
         playerControls = new PlayerInputActions();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -29,17 +34,36 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
         movement = playerControls.Player.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("MoveX", movement.x);
+        myAnimator.SetFloat("MoveY", movement.y);
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRender.flipX = true;
+        }
+        else
+        {
+            mySpriteRender.flipX = false;
+        }
     }
 
 
