0f2adbb96fbb23ab1d5f8f05e21dd4edd5bbd425
KapAmantay <kapyshev.2002@gmail.com>
2024-01-21 21:41:39 +0600

    enemy health

--- DiplomaProject/Assets/Scripts/Player/DamageSource.cs
+++ DiplomaProject/Assets/Scripts/Player/DamageSource.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class DamageSource : MonoBehaviour
 {
+    [SerializeField] private int damageAmount = 1;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<EnemyScript>())
-        {
-            Debug.Log("HIyyaaAAAA!");
-        }
-    }
 
+        if (other.gameObject.GetComponent<EnemyHealth>())
+        {
+            EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+            enemyHealth.TakeDamage(damageAmount);
+
+        }
+
+    }
 }
